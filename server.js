55f8a1278afa67fa5537b677f390511f03d7e852
jashwanthreddyb213 <chintureddy.b213@gmail.com>
2026-06-06 13:24:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const cors=require("cors");
+const dotenv=require("dotenv");
+const connectDB=require("./config/db");
+const userRoutes=require("./routes/userRoutes");
+const courseRoutes=require("./routes/courseRoute");
+const enrollmentRoute=require("./routes/enrollmentRoute");
+dotenv.config();
+connectDB();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use("/enrollments",enrollmentRoute);
+app.use("/users",userRoutes);
+app.use("/courses",courseRoutes);
+app.get("/",(req,res)=>{
+	res.send("API running");
+})
+const PORT=5000;
+app.listen(PORT,()=>{
+	console.log(`server running on port: ${PORT}`);
+})
