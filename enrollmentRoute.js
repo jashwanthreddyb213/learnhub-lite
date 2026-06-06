f1c174acd9e32f96a414353635faa9a844fc014a
jashwanthreddyb213 <chintureddy.b213@gmail.com>
2026-06-06 14:19:10 +0530

    Delete enrollmentRoute.js

--- enrollmentRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const {protect}=require("../middleware/authMiddleware");
-const {enrollCourse,myCourses} =require("../controllers/enrollmentController");
-const router=express.Router();
-router.post("/:id/enroll",protect,enrollCourse);
-router.get("/mycourses",protect,myCourses);
-module.exports=router;
