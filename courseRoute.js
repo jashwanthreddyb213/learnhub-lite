13dd54e3f2552fc3d44f5c71225b0131d9f63b33
jashwanthreddyb213 <chintureddy.b213@gmail.com>
2026-06-06 14:20:23 +0530

    Delete courseRoute.js

--- courseRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const {protect}=require("../middleware/authMiddleware");
-const {getCourses,addCourse}=require("../controllers/courseController");
-const router=express.Router();
-router.get("/",getCourses);
-router.post("/",protect,addCourse);
-module.exports=router;
