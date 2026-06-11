be7c03d8b0d598b9c4daad071fa3928438c39b53
jashwanthreddyb213 <chintureddy.b213@gmail.com>
2026-06-11 12:58:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,25 @@ app.use(express.json());
 app.use("/enrollments",enrollmentRoute);
 app.use("/users",userRoutes);
 app.use("/courses",courseRoutes);
-app.get("/",(req,res)=>{
-	res.send("API running");
-})
+console.log("1. Server file started");
+
+dotenv.config();
+
+console.log("2. Env loaded");
+
+connectDB();
+
+console.log("3. DB connection function called");
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`4. Server running on port ${PORT}`);
 });
+
+
+
