13e90cef96d9df2874036174ac5f2474540e8fc8
jashwanthreddyb213 <chintureddy.b213@gmail.com>
2026-06-11 12:10:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,7 @@ app.get("/",(req,res)=>{
 	res.send("API running");
 })
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
